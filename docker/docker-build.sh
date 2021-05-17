6cc034efe69d6150788cd3fa553e788decc00a2f
Michael Herger <michael@herger.net>
2021-05-17 09:03:38 +0200

    Build spotty/librespot Linux binaries in Docker
    Seems to be working fine on Linux hosts and Intel based macOS. Does NOT work on M1 based Mac, even when Docker is running in x86_64 mode.

---
+++ docker/docker-build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eux
+
+uname -a
+DESTDIR=/src/releases
+
+mkdir -p $DESTDIR/i386-linux
+rm -f $DESTDIR/i386-linux/*
+
+mkdir -p $DESTDIR/arm-linux
+rm -f $DESTDIR/arm-linux/*
+
+function build {
+	echo Building for $1 to $3...
+	cargo build --release --target $1 \
+		&& $2 /build/$1/release/spotty \
+		&& cp /build/$1/release/spotty $DESTDIR/$3
+}
+
+build arm-unknown-linux-gnueabihf arm-linux-gnueabihf-strip arm-linux/spotty-hf
+build aarch64-unknown-linux-gnu aarch64-linux-gnu-strip arm-linux/spotty-aarch64
+build x86_64-unknown-linux-musl strip i386-linux/spotty-x86_64
+build i686-unknown-linux-musl strip i386-linux/spotty
---
+++ xbuild.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+docker build -t spotty-cross - < docker/Dockerfile
+docker run -rm -v $PWD/target:/build -v $PWD:/src spotty-cross
+#cargo build
